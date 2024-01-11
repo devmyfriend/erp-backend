a20138a7d42606ecdbfa74990bbfc1bbb7c234eb
Sebastian Martinez <sebastiansilvan97@gmail.com>
2024-01-11 16:27:48 -0500

    ft: docs /pais

--- src/routes/pais.routes.js
+++ src/routes/pais.routes.js
@@ -4,8 +4,75 @@ import * as middleware from '../middlewares/express-validator.js';
 import { body, param } from 'express-validator';
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: País
+ *     description: Operaciones relacionadas con las colonias segun el país, estado, municipio y cp
+ *
+ *
+ */
+
+/**
+ * @swagger
+ * /api/v1/pais/:
+ *   get:
+ *     summary: Obtener una lista de países
+ *     tags: [País]
+ *     responses:
+ *       200:
+ *         description: Lista de países
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   ClavePais:
+ *                     type: string
+ *                     example: "ABW"
+ *                   Descripcion:
+ *                     type: string
+ *                     example: "Aruba"
+ */
+
 router.get('/', methods.obtenerPaises);
 
+/**
+ * @swagger
+ * /api/v1/pais/estados/{clave_pais}:
+ *   get:
+ *     tags:
+ *       - País
+ *     summary: Obtener estados por clave de país
+ *     parameters:
+ *       - in: path
+ *         name: clave_pais
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: clave del país
+ *     responses:
+ *       200:
+ *         description: Lista de estados del país especificado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   ClavePais:
+ *                     type: string
+ *                     example: "MEX"
+ *                   ClaveEstado:
+ *                     type: string
+ *                     example: "AGU"
+ *                   Nombre:
+ *                     type: string
+ *                     example: "Aguascalientes"
+ */
 router.get(
 	'/estados/:id',
 	param('id', 'El parametro debe ser un string')
@@ -16,6 +83,46 @@ router.get(
 	methods.obtenerEstadoPorPais,
 );
 
+/**
+ * @swagger
+ * /api/v1/pais/estados/colonias:
+ *   post:
+ *     tags:
+ *       - País
+ *     summary: Obtener colonias por código postal y clave de país
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *             schema:
+ *                 type: object
+ *                 properties:
+ *                   cp:
+ *                     type: number
+ *                     example: 77518
+ *                   clave_pais:
+ *                     type: string
+ *                     example: "ROO"
+ *     responses:
+ *       200:
+ *         description: Lista de colonias
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  type: object
+ *                  properties:
+ *                   ClaveColonia:
+ *                     type: number
+ *                     example: "7"
+ *                   nombre_colonia:
+ *                     type: string
+ *                     example: "AGU"
+ *                   Nombre:
+ *                     type: string
+ *                     example: "Paseos Chac Mool"
+ */
 router.post(
 	'/estados/colonias',
 	body('cp', 'El parametro cp debe ser un entero')
