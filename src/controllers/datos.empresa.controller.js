9f23e46e827b57e35394bb27af43c54a2d5eb305
Sebastian Martinez <sebastiansilvan97@gmail.com>
2024-01-22 14:19:12 -0500

    fix: /empresa/contactos

--- src/controllers/datos.empresa.controller.js
+++ src/controllers/datos.empresa.controller.js
@@ -284,6 +284,8 @@ const buscarContactosPorEntidadNegocioId = async (req, res) => {
 		if (contactos.length === 0) {
 			return res.status(404).json({ message: 'No hay contactos disponibles' });
 		}
+
+		return res.status(200).json(contactos)
 	} catch (error) {
 		console.error('Error al obtener los contactos:', error.message);
 		return res.status(500).json({ error: 'Internal Server Error' });
