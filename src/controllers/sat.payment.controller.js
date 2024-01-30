f2606a508509c527d9f7af5bacacadd988c66d86
cahuichLuis <cawy2000@hotmail.com>
2024-01-30 13:19:42 -0500

    ft: CRUD Payment Type

--- src/controllers/sat.payment.controller.js
+++ src/controllers/sat.payment.controller.js
@@ -1,5 +1,6 @@
 import { Connection as sequelize } from '../database/mariadb.database.js';
 import { paymentMethods } from '../models/sat.payment.methods.model.js';
+import { paymentType } from '../models/sat.payment.type.model.js';
 
 const createPaymentMethods = async (req, res) => {
 	const paymentBody = req.body;
@@ -94,8 +95,103 @@ const deletePaymentMethods = async (req, res) => {
 	}
 };
 
+const createPaymentType = async (req, res) => {
+	const paymentTypeBody = req.body;
+
+	try { 
+		const validatePaymentType = await paymentType.findOne({
+			where: {
+				ClaveMetodoPago: paymentTypeBody.ClaveMetodoPago,
+				Activo: 1,
+			},
+		});
+
+		if (validatePaymentType) {
+			return res
+				.status(400)
+				.json({ message: 'Ya existe un tipo de pago con esa clave' });
+		}
+
+		await paymentType.create(paymentTypeBody);
+
+		return res.status(200).json({ message: 'Se ha creado el tipo de pago' });
+	} catch (error) {
+		console.log('Error al crear el tipo de pago', error);
+		return res
+			.status(500)
+			.json({ message: 'Error al crear el tipo de pago' });
+	}
+}
+
+const updatedPaymentType = async (req, res) => {
+	const paymentBody =  req.body
+
+	try{
+		const payment = await paymentType.findOne({
+			where: {
+				ClaveMetodoPago: paymentBody.ClaveMetodoPago,
+				Activo: 1,
+			},
+		});
+
+		if (!payment) {
+			return res
+				.status(404)
+				.json({ message: 'No se encontro el tipo de pago' });
+		}
+
+		const updatedPayment = await payment.update(paymentBody);
+
+		return res
+			.status(200)
+			.json({
+				message: 'Se ha actualizado el tipo de pago',
+				data: updatedPayment,
+			});
+	} catch(error){
+		console.log('Error al actualizar el tipo de pago', error);
+		return res
+			.status(500)
+			.json({ message: 'Error al actualizar el tipo de pago' });
+	}
+}
+
+const deletePaymentType = async (req, res) => {
+	const id = req.params.ClaveMetodoPago;
+
+	try {
+		const payment = await paymentType.findOne({
+			where: {
+				ClaveMetodoPago: id,
+				Activo: 1,
+			},
+		});
+
+		if (!payment) {
+			return res
+				.status(404)
+				.json({ message: 'No se encontro el tipo de pago' });
+		}
+
+		payment.Activo = 0;
+		await payment.save();
+
+		return res
+			.status(200)
+			.json({ message: 'Se ha eliminado el tipo de pago' });
+	} catch (error) {
+		console.log('Error al eliminar el tipo de pago', error);
+		return res
+			.status(500)
+			.json({ message: 'Error al eliminar el tipo de pago' });
+	}
+};
+
 export const methods = {
 	createPaymentMethods,
 	updatePaymentMethods,
 	deletePaymentMethods,
+	createPaymentType,
+	updatedPaymentType,
+	deletePaymentType
 };
--- src/schemas/payment.methods.js
+++ src/schemas/payment.methods.js
@@ -5,14 +5,14 @@ export const createPaymentMethodsSchema = [
         .notEmpty()
         .withMessage('La clave del método de pago es requerida')
         .isString()
-        .withMessage('La clave del método de pago debe ser un string')
+        .withMessage('La clave del método de pago debe ser una cadena de texto')
         .isLength({ min: 3, max: 3 })
         .withMessage('La clave del método de pago debe tener 3 caracteres'),
     body('Descripcion')
         .notEmpty()
         .withMessage('La descripción del método de pago es requerida')
         .isString()
-        .withMessage('La descripción del método de pago debe ser un string'),
+        .withMessage('La descripción del método de pago debe ser una cadena de texto'),
     body('Bancarizado')
         .notEmpty()
         .withMessage('El campo bancarizado es requerido')
@@ -25,17 +25,46 @@ export const updatePaymentMethodsSchema = [
         .notEmpty()
         .withMessage('La clave del método de pago es requerida')
         .isString()
-        .withMessage('La clave del método de pago debe ser un string')
+        .withMessage('La clave del método de pago debe ser una cadena de texto')
         .isLength({ min: 3, max: 3 })
         .withMessage('La clave del método de pago debe tener 3 caracteres'),
     body('Descripcion')
         .optional()
         .isString()
-        .withMessage('La descripción del método de pago debe ser un string'),
+        .withMessage('La descripción del método de pago debe ser una cadena de texto'),
     body('Bancarizado')
         .optional()
         .isBoolean()
         .withMessage('El campo bancarizado debe ser un booleano'),
+]
+
+export const createPaymentTypeSchema = [
+    body('ClaveMetodoPago')
+        .notEmpty()
+        .withMessage('La clave del método de pago es requerida')
+        .isString()
+        .withMessage('La clave del método de pago debe ser una cadena de texto')
+        .isLength({ min: 3, max: 3})
+        .withMessage('La clave del método de pago debe tener 3 caracteres'),
+    body('Descripcion')
+        .notEmpty()
+        .withMessage('La descripción del método de pago es requerida')
+        .isString()
+        .withMessage('La descripción del método de pago debe ser una cadena de texto'),
+]
+
+export const updatePaymentTypeSchema = [
+    body('ClaveMetodoPago')
+        .notEmpty()
+        .withMessage('La clave del método de pago es requerida')
+        .isString()
+        .withMessage('La clave del método de pago debe ser una cadena de texto')
+        .isLength({ min: 3, max: 3})
+        .withMessage('La clave del método de pago debe tener 3 caracteres'),
+    body('Descripcion')
+        .optional()
+        .isString()
+        .withMessage('La descripción del método de pago debe ser una cadena de texto'),
 ]
 
 
