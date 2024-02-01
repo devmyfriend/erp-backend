b93ea7319888296ba9e4d7d7991f889e9febccd8
cahuichLuis <cawy2000@hotmail.com>
2024-02-01 12:02:46 -0500

    ft: CRUD SAT Products Services

--- src/controllers/catalogos.busquedas.controller.js
+++ src/controllers/catalogos.busquedas.controller.js
@@ -4,6 +4,7 @@ import { Colonias } from '../models/colonia.model.js';
 import { regimenFiscal } from '../models/sat.regimen.fiscal.model.js';
 import { Coin } from '../models/sat.type.coin.js';
 import { UsoCFDI } from '../models/sat.uso.cfdi.model.js';
+import ProductoServicio from '../models/sat.product.services.model.js';
 
 const getPostalCodes = async (req, res) => {
 	try {
@@ -343,6 +344,119 @@ const findCFDI = async (req, res) => {
 	});
 	return res.status(200).json(result);
 };
+
+const findProductServicesByCode = async (req, res) => {
+	const { code } = req.params;
+	try {
+		const data = await ProductoServicio.findAll({
+            where: { ClaveProductoServicio: { [Op.like]: '%' + code + '%' } },
+		});
+		if (!data) {
+			return res.status(404).json({ message: 'No hay datos disponibles' });
+		}
+		return res.status(200).json(data);
+	} catch (error) {
+		console.error('Error al obtener los datos del producto', error.message);
+		return res.status(500).json({ error: 'Error al obtener los datos' });
+	}
+};
+
+const findProductServicesByDescription = async (req, res) => {
+	const { descripcion } = req.params;
+
+	try {
+		const data = await ProductoServicio.findAll({
+			where: { Descripcion: { [Op.like]: descripcion + '%'} },
+		});
+		if (!data) {
+			return res.status(404).json({ message: 'No hay datos disponibles' });
+		}
+
+		return res.status(200).json(data);
+	} catch (error) {
+		console.error('Error al obtener los datos del producto', error.message);
+		return res.status(500).json({ error: 'Error al obtener los datos' });
+	} 
+}
+
+const findProductServicesByMatchWord = async (req, res) => {
+	const { palabra } = req.params;
+	try {
+		const data = await ProductoServicio.findAll({
+			where: { PalabrasSimilares: { [Op.like]: '%' + palabra + '%' } },
+		});
+		if (!data) {
+			return res.status(404).json({ message: 'No hay datos disponibles' });
+		}
+		return res.status(200).json(data);
+	} catch (error) {
+		console.error('Error al obtener los datos del producto', error.message);
+		return res.status(500).json({ error: 'Error al obtener los datos' });
+	}
+}
+
+const createProductServices = async (req, res) => {
+	const productServicesBody = req.body;
+	try {
+
+		const validateProductServices = await ProductoServicio.findOne({
+			where: { ClaveProductoServicio: productServicesBody.ClaveProductoServicio, Activo: 1 },
+		});
+
+		if (validateProductServices) {
+			return res
+				.status(409)
+				.json({ error: 'La clave del producto/servicio ya esta en uso ' });
+		}
+
+		await ProductoServicio.create(productServicesBody);
+		return res.status(200).json({ success: true, message: 'Producto/Servicio creado' });
+	} catch (error) {
+		console.error('Error al crear el producto/servicio', error.message);
+		return res.status(500).json({ error: 'Error al crear el producto/servicio' });
+	}
+}
+
+const updateProductServices = async (req, res) => {
+	const productServicesBody = req.body;
+	try {
+		const [updated] = await ProductoServicio.update(productServicesBody, {
+			where: { ClaveProductoServicio: productServicesBody.ClaveProductoServicio, Activo: 1 },
+		});
+
+		if (!updated) {
+			return res.status(404).json({ error: 'Producto/Servicio no encontrado' });
+		}
+
+		return res.status(200).json({ success: true, message: 'Producto/Servicio actualizado' });
+	} catch (error) {
+		console.error('Error al actualizar el producto/servicio', error.message);
+		return res.status(500).json({ error: 'Error al actualizar el producto/servicio' });
+	}
+}
+
+const deleteProductServices = async (req, res) => {
+	const { ClaveProductoServicio } = req.body;
+
+	try {
+		const product = await ProductoServicio.findOne({
+			where: { ClaveProductoServicio, Activo: 1 },
+		});
+
+		if (!product) {
+			return res.status(404).json({ error: 'Producto/Servicio no encontrado' });
+		}
+
+		await ProductoServicio.update({ Activo: false }, { where: { ClaveProductoServicio } });
+
+		return res.status(200).json({ success: true, message: 'Producto/Servicio borrado' });
+	} catch (error) {
+		console.error('Error al borrar el producto/servicio', error.message);
+		return res.status(500).json({ error: 'Error al borrar el producto/servicio' });
+	}
+}
+
+
 export const methods = {
 	getPostalCodes,
 	findPostalCodes,
@@ -362,4 +476,10 @@ export const methods = {
 	updateUsoCFDI,
 	deleteCFDI,
 	findCFDI,
+	findProductServicesByCode,
+	findProductServicesByDescription,
+	findProductServicesByMatchWord,
+	createProductServices,
+	updateProductServices,
+	deleteProductServices
 };
