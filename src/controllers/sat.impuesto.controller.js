f556a239d5266cc95c5b2335f79cb5ef611a4d7c
LaloG19 <21362134@utcancun.edu.mx>
2024-04-25 12:41:45 -0500

    up: Eliminación de comentarios de depuración

--- src/controllers/sat.impuesto.controller.js
+++ src/controllers/sat.impuesto.controller.js
@@ -22,27 +22,6 @@ const findAll = async (req, res) => {
     }
 };
 
-/* const findByName = async (req, res) => {
-    const name = req.body.Nombre;
-    try {
-
-        const data = await finderByName(name);
-        if (!data.exist) {
-            return res.status(404).json({
-                status: 404,
-                error: 'No se encontraron valores',
-            });
-        }
-
-        return res.status(200).json({ message: 'Datos encontrados', data: data.data });
-    } catch (error) {
-        console.error(
-            'Error al obtener los datos del impuesto',
-            error.message,
-        );
-        return res.status(500).json({ error: 'Error al obtener los datos' });
-    }
-}; */
 const findByName = async (req, res) => {
     const name = req.body.Nombre;
     try {
