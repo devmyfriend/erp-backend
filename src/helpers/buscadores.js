963552fe972b64163e8a43a179552b48210e48e7
LaloG19 <21362134@utcancun.edu.mx>
2024-07-18 14:05:41 -0500

    add: Se añadió el helper de buscadores

---
+++ src/helpers/buscadores.js
@@ -0,0 +1,47 @@
+import { Op } from 'sequelize';
+import { 
+    ProductosServicios, 
+} from '../models/index.js';
+
+const paginado = parseInt(process.env.RegistrosPorPagina) || 10;
+
+const manejadorDBError = error => {
+  console.error(error);
+  return { error: 'Error interno en el servidor' };
+};
+
+const searchItems = async (modelo, condicion, pagina) => {
+  try {
+    const data = await modelo.findAndCountAll({
+      where: condicion,
+      limit: paginado,
+      offset: (pagina - 1) * paginado,
+    });
+    const TotalPaginas = Math.ceil(data.count / paginado);
+    if (pagina > TotalPaginas) {
+        return { existe: false, data: [] };
+    }
+    return { existe: true, data: {
+        TotalRegistros: data.count,
+        PaginaActual: pagina,
+        TotalPaginas,
+        Datos: data.rows,
+    } };
+  } catch (error) {
+    return manejadorDBError(error);
+  }
+};
+
+export const buscadorProductosServiciosPorDescripcion = async (descripcion, pagina) => {
+    const condicion = {
+        Descripcion: { [Op.like]: `%${descripcion}%` },
+    };
+    return searchItems(ProductosServicios, condicion, pagina);
+}
+
+export const buscadorProductosServiciosPorPalabra = async (palabra, pagina) => {
+    const condicion = {
+        PalabrasSimilares: { [Op.like]: `%${palabra}%` },
+    };
+    return searchItems(ProductosServicios, condicion, pagina);
+}
