77017077bfb2d7ccc2a294cba9e3378737eebff9
LaloG19 <21362134@utcancun.edu.mx>
2024-07-29 12:23:06 -0500

    feat: Integración de la bitacora en los endpoints de "satTipoComprobante"

--- src/controllers/satTipoComprobante.controller.js
+++ src/controllers/satTipoComprobante.controller.js
@@ -1,5 +1,6 @@
 import { TiposComprobantes } from '../models/satTipoComprobante.model.js';
 import { validaTiposComprobantes } from '../middlewares/finders/index.js';
+import { Bitacora } from '../helpers/logs/log.js';
 
 const obtenerTiposComprobantes = async (req, res) => {
     try {
@@ -19,80 +20,104 @@ const obtenerTiposComprobantes = async (req, res) => {
 		});
     } catch (error) {
 		console.error(error);
+		Bitacora('obtenerTiposComprobantes', error);
 		res.status(500).send({
 			status: "Error",
 			message: "Error al obtener los tipos de comprobante",
-			Error: error
 		});
     }
 };
 
 const crearTiposComprobantes = async (req, res) => {
-	const tiposComprobantesBody = req.body;
-	const idExistente = await validaTiposComprobantes(tiposComprobantesBody.ClaveTipoDeComprobante);
-
-	if (idExistente.existe) {
-		return res.status(409).json({
-			status: 409,
-			error: 'El tipo de comprobante ya existe',
+	try{
+		const tiposComprobantesBody = req.body;
+		const idExistente = await validaTiposComprobantes(tiposComprobantesBody.ClaveTipoDeComprobante);
+	
+		if (idExistente.existe) {
+			return res.status(409).json({
+				status: 409,
+				error: 'El tipo de comprobante ya existe',
+			});
+		}
+	
+		return res
+			.status(200)
+			.send({ 
+				status: "OK",
+				message: "Tipo de comprobante creado correctamente",
+				data: await TiposComprobantes.create(tiposComprobantesBody),
+			});
+	}catch(error){
+		Bitacora('crearTiposComprobantes', error);
+		res.status(500).send({
+			status: "Error",
+			message: "Error al crear el tipo de comprobante",
 		});
 	}
-
-	return res
-		.status(200)
-		.send({ 
-			status: "OK",
-			message: "Tipo de comprobante creado correctamente",
-			data: await TiposComprobantes.create(tiposComprobantesBody),
-		});
 };
 
 const actualizarTiposComprobantes = async (req, res) => {
-	const tiposComprobantesBody = await req.body;
-	const idExistente = await validaTiposComprobantes(tiposComprobantesBody.ClaveTipoDeComprobante)
-
-	if (!idExistente.existe) {
-		return res.status(404).json({
-			status: 404,
-			error: 'El tipo de comprobante no existe',
+	try{
+		const tiposComprobantesBody = await req.body;
+		const idExistente = await validaTiposComprobantes(tiposComprobantesBody.ClaveTipoDeComprobante)
+	
+		if (!idExistente.existe) {
+			return res.status(404).json({
+				status: 404,
+				error: 'El tipo de comprobante no existe',
+			});
+		}
+	
+		await TiposComprobantes.update(tiposComprobantesBody, {
+			where: { ClaveTipoDeComprobante: tiposComprobantesBody.ClaveTipoDeComprobante },
+		})
+	
+		return res
+			.status(200)
+			.send({ 
+				status: "OK",
+				message: "Tipo de comprobante actualizado correctamente",
+				data: tiposComprobantesBody,
+			});
+	}catch(error){
+		Bitacora('actualizarTiposComprobantes', error);
+		res.status(500).send({
+			status: "Error",
+			message: "Error al actualizar el tipo de comprobante",
 		});
 	}
-
-	await TiposComprobantes.update(tiposComprobantesBody, {
-		where: { ClaveTipoDeComprobante: tiposComprobantesBody.ClaveTipoDeComprobante },
-	})
-
-	return res
-		.status(200)
-		.send({ 
-			status: "OK",
-			message: "Tipo de comprobante actualizado correctamente",
-			data: tiposComprobantesBody,
-		});
 };
 
 const borrarTiposComprobantes = async (req, res) => {
-	const id = req.body.ClaveTipoDeComprobante;
-	const idExistente = await validaTiposComprobantes(id);
-
-	if (!idExistente.existe) {
-		return res.status(404).json({
-			status: 404,
-			error: 'El tipo de comprobante no existe',
+	try{
+		const id = req.body.ClaveTipoDeComprobante;
+		const idExistente = await validaTiposComprobantes(id);
+	
+		if (!idExistente.existe) {
+			return res.status(404).json({
+				status: 404,
+				error: 'El tipo de comprobante no existe',
+			});
+		}
+	
+		await TiposComprobantes.update({ Borrado: 1 }, {
+			where: { ClaveTipoDeComprobante: id },
+		})
+	
+		return res
+			.status(200)
+			.send({ 
+				status: "OK",
+				message: "Tipo de comprobante eliminado correctamente",
+				data: id,
+			});
+	}catch(error){
+		Bitacora('borrarTiposComprobantes', error);
+		res.status(500).send({
+			status: "Error",
+			message: "Error al borrar el tipo de comprobante",
 		});
 	}
-
-	await TiposComprobantes.update({ Borrado: 1 }, {
-		where: { ClaveTipoDeComprobante: id },
-	})
-
-	return res
-		.status(200)
-		.send({ 
-			status: "OK",
-			message: "Tipo de comprobante eliminado correctamente",
-			data: id,
-		});
 };
 
 export const methods = {
